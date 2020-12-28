a8650ad4253fab598e2073993276a30996808224
おーもと <vitamin29290209@gmail.com>
2020-12-28 22:28:29 +0900

    ブログ登録処理

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
 
 class BlogController extends Controller
 {
@@ -29,5 +30,34 @@ class BlogController extends Controller
             return redirect(route('blogs'));
         }
         return view('blog.detail',['blog' => $blog]);
+    }
+    /**
+     * ブログ登録画面を表示する
+     *
+     * @return view
+    */
+    public function showCreate(){
+        return view('blog.form');
+    }
+    /**
+     * ブログを登録する
+     *
+     * @return view
+    */
+    public function exeStore(BlogRequest $request){
+        //ブログのデータを受け取る
+        $inputs = $request->all();
+        \DB::beginTransaction();
+        try{
+            //ブログを登録
+            Blog::create($inputs);
+            \DB::commit();
+        } catch(\Throwable $e){
+            \DB::rollback();
+            abort(500);
+        }
+
+        \Session::flash('err_msg','ブログを登録しました');
+        return redirect(route('blogs'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 //ブログ一覧を表示
 Route::get('/', 'BlogController@showList')->name('blogs');
+
+//ブログ登録画面を表示
+Route::get('/blog/create', 'BlogController@showCreate')->name('create');
+
+//ブログ登録
+Route::post('/blog/store', 'BlogController@exeStore')->name('store');
+
 //ブログ詳細画面を表示
 Route::get('/blog/{id}', 'BlogController@showDetail')->name('show');
