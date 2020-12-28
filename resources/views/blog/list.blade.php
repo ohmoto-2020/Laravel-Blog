8b4589358cd951b4bb0ac37b334709c395b5e47f
おーもと <vitamin29290209@gmail.com>
2020-12-28 23:53:49 +0900

    form閉じタグ忘れ

--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -22,8 +22,9 @@
         <td>{{ $blog->updated_at }}</td>
         <td><button type="button" class="btn btn-primary" onclick="location.href='/blog/edit/{{ $blog->id }}'">編集</button></td>
         <form method="POST" action="{{ route('delete',$blog->id) }}" onSubmit="return checkDelete()">
-        @csrf
-        <td><button type="submit" class="btn btn-primary" onclick= >削除</button></td>
+          @csrf
+          <td><button type="submit" class="btn btn-primary" onclick= >削除</button></td>
+        </form>
       </tr>
       @endforeach
     </table>
